9c36d85a47ddccecd18c92e0c3c06ee3ef8883f3
Shinde Chaitanya Adinath <46563731+chaitu1410@users.noreply.github.com>
2020-11-21 10:45:51 +0530

    needed commit 7

---
+++ client/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace client.Data
+{
+    class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Database.mdf;Integrated Security=True");
+        }
+    }
+}
