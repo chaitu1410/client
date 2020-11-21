7d0eb8bbd629a96827147a1896948bf0139013d1
Shinde Chaitanya Adinath <46563731+chaitu1410@users.noreply.github.com>
2020-11-21 10:13:08 +0530

    needed commit 2

---
+++ client/Data/Models/Item.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace client.Data.Models
+{
+    class Item
+    {
+        public int ItemId { get; set; }
+        public string Name { get; set; }
+    }
+}
