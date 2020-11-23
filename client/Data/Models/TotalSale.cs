6daa984b9a1b7e54bd4e52acf857725cf9e34dd0
Shinde Chaitanya Adinath <46563731+chaitu1410@users.noreply.github.com>
2020-11-23 14:14:08 +0530

    Total sale model and Repository created

---
+++ client/Data/Models/TotalSale.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace client.Data
+{
+    class TotalSale
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(10, 2)")]
+        public decimal CashAmount { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(10, 2)")]
+        public decimal SaleReturnAmount { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(10, 2)")]
+        public decimal CardAmount { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(10, 2)")]
+        public decimal ExtraAmount { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(10, 2)")]
+        public decimal TotalSaleAmount { get; set; }
+
+        [Timestamp]
+        public DateTime Date { get; set; }
+
+    }
+}
