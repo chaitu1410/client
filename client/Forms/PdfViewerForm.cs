739c0371a53b7e09d54ce5c808d97b7a73c463f4
Amruta-Totala <amrutatotala0806@gmail.com>
2020-12-01 22:07:24 +0530

    PdfViewer form added

---
+++ client/Forms/PdfViewerForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace client.Forms
+{
+    public partial class PdfViewerForm : Form
+    {
+        public PdfViewerForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
